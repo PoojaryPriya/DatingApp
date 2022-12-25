af08b09477ac103f7201f07c73cee7b89c592b4e
PriyaPoojary26 <priyapoojary336@gmail.com>
2022-12-25 13:17:32 +0530

    End of section3

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,6 @@
 using System.Security.Cryptography;
 using API.Data;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;    
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,21 +17,11 @@ builder.Services.AddDbContext<DataContext>(opt=>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 
-
-
-var app = builder.Build();
-
+var app= builder.Build();
 // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
-
-// app.UseHttpsRedirection();
-
-// app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
